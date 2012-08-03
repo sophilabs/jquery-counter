39a9254cdccc76503d49fde08c4cd2895aad40fb
Pablo Ricco <pricco@gmail.com>
2012-08-03 18:34:15 -0300

    replace parseFloat() by parseInt(x, 10) - Thanks @grilix

--- src/jquery.counter.js
+++ src/jquery.counter.js
@@ -67,8 +67,7 @@
         return this.each(function() {
             var e = $(this);
             var data = e.data('counter') || {};
-            //WARN: Don't use parseInt => parseInt('09') == 0
-            data.interval = parseFloat(options.interval || e.attr('data-interval') || '1000');
+            data.interval = parseInt(options.interval || e.attr('data-interval') || '1000', 10);
             data.down = ( options.direction || e.attr('data-direction') || 'down') == 'down';
             data.parts = [];
             var initial = (options.initial || e.text()).split(/([^0-9]+)/);
